115afcaa38060159ae9389119337c95159c5e2b2
Heath Hodgert <heath.hodgert@viewpoint.com>
2017-09-21 11:47:44 -0700

    Initial app

---
+++ RunEveryWhere.UnitTest/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace RunEveryWhere.UnitTest
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
---
+++ RunEveryWhere/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace RunEveryWhere
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
